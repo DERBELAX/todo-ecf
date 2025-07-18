0dd0e76139e9dbb7f740cf84d6f0939e9f8c3a25
DERBELAX <164332911+DERBELAX@users.noreply.github.com>
2025-07-18 18:56:20 +0200

    up apptest

--- back/src/__test__/db.test.js
+++ back/src/__test__/db.test.js
@@ -11,6 +11,7 @@ describe('DB config', () => {
     expect(mongoose.connect).toHaveBeenCalled();
   });
 
+
   test('affiche une erreur en cas d’échec', async () => {
     mongoose.connect.mockRejectedValueOnce(new Error('Connection failed'));
     const consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
@@ -18,4 +19,27 @@ describe('DB config', () => {
     expect(consoleSpy.mock.calls[0][0]).toMatch(/impossible de se connecter/i);
     consoleSpy.mockRestore();
   });
+
+  test('affiche un message de succès dans la console', async () => {
+  const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+  await connectDB();
+  expect(consoleSpy).toHaveBeenCalledWith(expect.stringMatching(/succès/i));
+  consoleSpy.mockRestore();
+  });
+
+  test('connectDB retourne une promesse', () => {
+  const result = connectDB();
+  expect(result).toBeInstanceOf(Promise);
+  });
+
+
+  test('appelle mongoose.connect avec le bon URI', async () => {
+  await connectDB();
+
+  const expectedURI = expect.stringMatching(/^mongodb\+srv:\/\/.*@.*\.mongodb\.net\/.*\?/);
+  expect(mongoose.connect).toHaveBeenCalledWith(expectedURI, expect.any(Object));
+});
+
+
+
 });
